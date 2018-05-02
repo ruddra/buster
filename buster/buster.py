1de2d057da3fb154b4c4f673556e7bef8cf00c9e
Arnab Kumar Shil <ruddra90@gmail.com>
2018-05-02 17:29:46 +0600

    Update buster.py

--- buster/buster.py
+++ buster/buster.py
@@ -40,20 +40,25 @@ def main():
 
     if arguments['generate']:
         command = ("wget "
-                   "--recursive "             # follow links to download entire site
+                   "--recursive "             # recursive
                    "--convert-links "         # make links relative
                    "--page-requisites "       # grab everything: css / inlined images
                    "--no-parent "             # don't go to parent level
-                   "--directory-prefix {1} "  # download contents to static/ folder
+                   # download contents to static/ folder
+                   "--directory-prefix {1} "
                    "--no-host-directories "   # don't create domain named folder
                    "--restrict-file-name=unix "  # don't escape query string
                    "{0}").format(arguments['--domain'], static_path)
         os.system(command)
-        
+
         if arguments['--domain']:
-          domain = arguments['--domain']
+            domain = arguments['--domain']
+            if domain.startswith('http//') or domain.startswith('https//'):
+                pass
+            else:
+                domain = 'http://{}'.format(domain)
         else:
-          domain = 'http://localhost:2368'
+            domain = 'http://localhost:2368'
         target_domain = arguments['--target_domain']
 
         # remove query string since Ghost 0.4
@@ -63,12 +68,24 @@ def main():
                 if file_regex.match(filename):
                     newname = re.sub(r'\?.*', '', filename)
                     print "Rename", filename, "=>", newname
-                    os.rename(os.path.join(root, filename), os.path.join(root, newname))
+                    os.rename(os.path.join(root, filename),
+                              os.path.join(root, newname))
 
         # remove superfluous "index.html" from relative hyperlinks found in text
         abs_url_regex = re.compile(r'^(?:[a-z]+:)?//', flags=re.IGNORECASE)
+
         def fixLinks(text, parser):
-            d = PyQuery(bytes(bytearray(text, encoding='utf-8')), parser=parser)
+            d = PyQuery(bytes(bytearray(text, encoding='utf-8')),
+                        parser=parser)
+            for element in d('link'):
+                e = PyQuery(element)
+                href = e.attr('href')
+                if href:
+                    if href.find(domain) > -1:
+                        new_href = href.split(domain)[-1]
+                        new_href = '{}{}'.format(target_domain, new_href)
+                        e.attr('href', new_href)
+                        print "\t", "fixed link ", href, "=> ", new_href
             for element in d('a'):
                 e = PyQuery(element)
                 href = e.attr('href')
@@ -76,83 +93,74 @@ def main():
                     if href.find(domain) > -1:
                         new_href = href.split(domain)[-1]
                         e.attr('href', new_href)
-                        print "\t", "Fixed ", href ,"=> ", new_href
+                        print "\t", "Fixed ", href, "=> ", new_href
                 if href and not abs_url_regex.search(href):
-                    new_href = re.sub(r'rss/index\.html$', 'rss/index.rss', href)
+                    new_href = re.sub(r'rss/index\.html$',
+                                      'rss/index.rss', href)
                     new_href = re.sub(r'/index\.html$', '/', new_href)
                     e.attr('href', new_href)
                     print "\t", href, "=>", new_href
             if parser == 'html':
                 return d.html(method='html').encode('utf8')
             return d.__unicode__().encode('utf8')
-            
-        def fix_share_links(text,parser):
+
+        def fix_share_links(text, parser):
             filetext = text.decode('utf8')
-            td_regex = re.compile(target_domain + '|' )
-            
+            td_regex = re.compile(target_domain + '|')
+
             assert target_domain, "target domain must be specified --target_domain=<http://your-host-url>"
-            d = PyQuery(bytes(bytearray(filetext, encoding='utf-8')), parser=parser)
+            d = PyQuery(
+                bytes(bytearray(filetext, encoding='utf-8')), parser=parser)
             for share_class in ['.share_links a']:
-                print "share_class : ", share_class
                 for element in d(share_class):
                     e = PyQuery(element)
-                    print "element : ", e
                     href = e.attr('href')
-                    print "href : ", href
-                    print "domain : ", domain
-                    print "target_domain : ", target_domain
                     new_href = re.sub(domain, target_domain, href)
                     e.attr('href', new_href)
                     print "\t", href, "=>", new_href
             if parser == 'html':
                 return d.html(method='html').encode('utf8')
             return d.__unicode__().encode('utf8')
-           
-        def fix_meta_url_links(text,parser):
+
+        def fix_meta_url_links(text, parser):
             filetext = text.decode('utf8')
-            td_regex = re.compile(target_domain + '|' )
-            
+            td_regex = re.compile(target_domain + '|')
+
             assert target_domain, "target domain must be specified --target_domain=<http://your-host-url>"
-            d = PyQuery(bytes(bytearray(filetext, encoding='utf-8')), parser=parser)
-            for share_class in ['meta[property="og:url"], meta[name="twitter:url"]']:
-                print "share_class : ", share_class
+            d = PyQuery(
+                bytes(bytearray(filetext, encoding='utf-8')), parser=parser)
+            for share_class in ['meta[property="og:url"], meta[name="twitter:url"]', 'meta[property="og:url"]', 'meta[name="twitter:url"]']:
                 for element in d(share_class):
                     e = PyQuery(element)
-                    print "element : ", e
                     href = e.attr('content')
-                    print "href : ", href
-                    print "domain : ", domain
-                    print "target_domain : ", target_domain
                     new_href = re.sub(domain, target_domain, href)
                     e.attr('content', new_href)
-                    print "\t", href, "=>", new_href
+                    print "\t meta fixed", href, "=>", new_href
             if parser == 'html':
                 return d.html(method='html').encode('utf8')
             return d.__unicode__().encode('utf8')
 
-        def fix_meta_image_links(text,parser):
+        def fix_meta_image_links(text, parser):
             filetext = text.decode('utf8')
-            td_regex = re.compile(target_domain + '|' )
-            
+            td_regex = re.compile(target_domain + '|')
+
             assert target_domain, "target domain must be specified --target_domain=<http://your-host-url>"
-            d = PyQuery(bytes(bytearray(filetext, encoding='utf-8')), parser=parser)
-            for share_class in ['meta[property="og:image"], meta[name="twitter:image"]']:
+            d = PyQuery(
+                bytes(bytearray(filetext, encoding='utf-8')), parser=parser)
+            for share_class in ['meta[property="og:image"]', 'meta[name="twitter:image"]']:
                 print "share_class : ", share_class
                 for element in d(share_class):
                     e = PyQuery(element)
-                    print "element : ", e
                     href = e.attr('content')
-                    print "href : ", href
-                    print "domain : ", domain
-                    content_target_domain = target_domain.replace("/static", "")
-                    print "target_domain : ", content_target_domain
+                    content_target_domain = target_domain.replace(
+                        "/static", "")
                     new_href = re.sub(domain, content_target_domain, href)
                     e.attr('content', new_href)
-                    print "\t", href, "=>", new_href
+                    print "\t fix image link", href, "=>", new_href
             if parser == 'html':
                 return d.html(method='html').encode('utf8')
             return d.__unicode__().encode('utf8')
-            
+
         # fix links in all html files
         for root, dirs, filenames in os.walk(static_path):
             for filename in fnmatch.filter(filenames, "*.html"):
@@ -160,16 +168,17 @@ def main():
                 parser = 'html'
                 if root.endswith("/rss"):  # rename rss index.html to index.rss
                     parser = 'xml'
-                    newfilepath = os.path.join(root, os.path.splitext(filename)[0] + ".rss")
+                    newfilepath = os.path.join(
+                        root, os.path.splitext(filename)[0] + ".rss")
                     os.rename(filepath, newfilepath)
                     filepath = newfilepath
                 with open(filepath) as f:
                     filetext = f.read().decode('utf8')
                 print "fixing links in ", filepath
                 newtext = fixLinks(filetext, parser)
-                newtext = fix_share_links(newtext,parser)
-                newtext = fix_meta_url_links(newtext,parser)
-                newtext = fix_meta_image_links(newtext,parser)
+                newtext = fix_share_links(newtext, parser)
+                newtext = fix_meta_url_links(newtext, parser)
+                newtext = fix_meta_image_links(newtext, parser)
                 with open(filepath, 'w') as f:
                     f.write(newtext)
 
@@ -180,7 +189,7 @@ def main():
         port = arguments.get('--port')
         if not port:
             port = 9000
-        httpd = SocketServer.TCPServer(("",int(port)), Handler)
+        httpd = SocketServer.TCPServer(("", int(port)), Handler)
 
         print "Serving at port {}".format(port)
         # gracefully handle interrupt here
@@ -218,7 +227,8 @@ def main():
         # Add README
         file_path = os.path.join(static_path, 'README.md')
         with open(file_path, 'w') as f:
-            f.write('# Blog\nPowered by [Ghost](http://ghost.org) and [Buster](https://github.com/axitkhurana/buster/).\n')
+            f.write(
+                '# Blog\nPowered by [Ghost](http://ghost.org) and [Buster](https://github.com/axitkhurana/buster/).\n')
 
         print "All set! You can generate and deploy now."
 
@@ -231,7 +241,7 @@ def main():
 
         origin = repo.remotes.origin
         repo.git.execute(['git', 'push', '-u', origin.name,
-                         repo.active_branch.name])
+                          repo.active_branch.name])
         print "Good job! Deployed to Github Pages."
 
     elif arguments['add-domain']:
@@ -246,6 +256,7 @@ def main():
 
     else:
         print __doc__
+
 
 if __name__ == '__main__':
     main()
